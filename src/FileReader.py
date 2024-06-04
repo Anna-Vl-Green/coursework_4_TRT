01a9145c02fd4af089bcf457c9746a32f64dead5
Anna Green <annagreen.photo@gmail.com>
2024-06-04 23:23:13 +0400

    Добавлен класс FileReader со всеми необходимыми методами

---
+++ src/FileReader.py
@@ -0,0 +1,26 @@
+import os
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class FileReader:
+
+    def __init__(self, directory="data"):
+
+        self.files = []
+        self.data_list = []
+        self.directory = directory
+
+    def find_files(self):
+
+        self.files = os.listdir(self.directory)
+
+    def read_files(self):
+        if len(self.files) > 0:
+            for file in self.files:
+                with open(os.path.join(self.directory, file), "r") as f:
+                    self.data_list[file] = json.loads(file)
+        else:
+            logging.info(f'Выполненные операции отсутствуют')
