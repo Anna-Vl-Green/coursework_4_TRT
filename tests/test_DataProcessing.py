f03a7fd1432d7216040f9f7fb6392088781acdf6
Anna Green <annagreen.photo@gmail.com>
2024-06-06 18:17:27 +0400

    Написаны тесты для функций обработки данных (за исключением функции создания сообщений)

--- tests/test_DataProcessing.py
+++ tests/test_DataProcessing.py
@@ -1,1 +1,448 @@
-def test_
+import pytest
+from src.DataProcessing import DataProcessing
+from src.FileReader import FileReader
+
+
+test_data = [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+              "amount": "31957.58",
+              "currency": {
+                "name": "руб.",
+                "code": "RUB"
+              }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+          },
+        {
+            "id": 414894334,
+            "state": "EXECUTED",
+            "date": "2019-06-30T15:11:53.136004",
+            "operationAmount": {
+                "amount": "95860.47",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 59956820797131895975",
+            "to": "Счет 43475624104328495820"
+        },
+        {},
+        {
+            "id": 509552992,
+            "state": "EXECUTED",
+            "date": "2019-04-19T12:02:30.129240",
+            "operationAmount": {
+                "amount": "81513.74",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Maestro 9171987821259925",
+            "to": "МИР 2052809263194182"
+        },
+        {
+            "id": 596914981,
+            "state": "EXECUTED",
+            "date": "2018-04-16T17:34:19.241289",
+            "operationAmount": {
+                "amount": "65169.27",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1813166339376336",
+            "to": "Счет 97848259954268659635"
+        },
+        {
+            "id": 200634844,
+            "state": "CANCELED",
+            "date": "2018-02-13T04:43:11.374324",
+            "operationAmount": {
+                "amount": "42210.20",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 33355011456314142963",
+            "to": "Счет 45735917297559088682"
+        },
+        {
+            "id": 482520625,
+            "state": "EXECUTED",
+            "date": "2019-11-13T17:38:04.800051",
+            "operationAmount": {
+                "amount": "62814.53",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 38611439522855669794",
+            "to": "Счет 46765464282437878125"
+        },
+         {
+            "id": 667307132,
+            "state": "EXECUTED",
+            "date": "2019-07-13T18:51:29.313309",
+            "operationAmount": {
+                "amount": "97853.86",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 1308795367077170",
+            "to": "Счет 96527012349577388612"
+        }
+    ]
+
+@pytest.fixture(scope="module")
+def data_process():
+    """
+    Инициализирует объект класса DataProcessing для тестирования функционала модуля.
+    :return:
+    """
+    return DataProcessing()
+
+
+def test_data_screening_empty(data_process):
+    """
+    Тестирует функционал вывода системного сообщения об отсутствии успешных операций.
+    """
+    data_process.data_screening([])
+
+    assert data_process.executed_operations == []
+
+
+def test_data_screening(data_process):
+    """
+    Тестирует функционал метода фильтрации данных об операциях (data_screening).
+    :param file_reader.data_list: list[dict]
+    """
+    correct_data = [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 414894334,
+            "state": "EXECUTED",
+            "date": "2019-06-30T15:11:53.136004",
+            "operationAmount": {
+                "amount": "95860.47",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 59956820797131895975",
+            "to": "Счет 43475624104328495820"
+        },
+        {
+            "id": 509552992,
+            "state": "EXECUTED",
+            "date": "2019-04-19T12:02:30.129240",
+            "operationAmount": {
+                "amount": "81513.74",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Maestro 9171987821259925",
+            "to": "МИР 2052809263194182"
+        },
+        {
+            "id": 596914981,
+            "state": "EXECUTED",
+            "date": "2018-04-16T17:34:19.241289",
+            "operationAmount": {
+                "amount": "65169.27",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1813166339376336",
+            "to": "Счет 97848259954268659635"
+        },
+        {
+            "id": 482520625,
+            "state": "EXECUTED",
+            "date": "2019-11-13T17:38:04.800051",
+            "operationAmount": {
+                "amount": "62814.53",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 38611439522855669794",
+            "to": "Счет 46765464282437878125"
+        },
+        {
+            "id": 667307132,
+            "state": "EXECUTED",
+            "date": "2019-07-13T18:51:29.313309",
+            "operationAmount": {
+                "amount": "97853.86",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 1308795367077170",
+            "to": "Счет 96527012349577388612"
+        }
+    ]
+    data_process.data_screening(test_data)
+
+    assert data_process.executed_operations == correct_data
+
+
+def test_data_sort_empty(data_process):
+    """
+    Тестирует функционал вывода системного сообщения об отсутствии данных для сортировки.
+    """
+    data_process.data_screening([])
+    data_process.data_sort()
+
+
+    assert data_process.sorted_operations == []
+
+
+def test_data_sort(data_process):
+    """
+    Тестирует функционал метода сортировки данных об операциях (data_sort).
+    """
+    correct_data = [
+        {
+            "id": 482520625,
+            "state": "EXECUTED",
+            "date": "2019-11-13T17:38:04.800051",
+            "operationAmount": {
+                "amount": "62814.53",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 38611439522855669794",
+            "to": "Счет 46765464282437878125"
+        },
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 667307132,
+            "state": "EXECUTED",
+            "date": "2019-07-13T18:51:29.313309",
+            "operationAmount": {
+                "amount": "97853.86",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 1308795367077170",
+            "to": "Счет 96527012349577388612"
+        },
+        {
+            "id": 414894334,
+            "state": "EXECUTED",
+            "date": "2019-06-30T15:11:53.136004",
+            "operationAmount": {
+                "amount": "95860.47",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 59956820797131895975",
+            "to": "Счет 43475624104328495820"
+        },
+        {
+            "id": 509552992,
+            "state": "EXECUTED",
+            "date": "2019-04-19T12:02:30.129240",
+            "operationAmount": {
+                "amount": "81513.74",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Maestro 9171987821259925",
+            "to": "МИР 2052809263194182"
+        },
+        {
+            "id": 596914981,
+            "state": "EXECUTED",
+            "date": "2018-04-16T17:34:19.241289",
+            "operationAmount": {
+                "amount": "65169.27",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1813166339376336",
+            "to": "Счет 97848259954268659635"
+        }
+    ]
+
+    data_process.data_screening(test_data)
+    data_process.data_sort()
+
+    assert data_process.sorted_operations == correct_data
+
+
+def test_make_output_list(data_process):
+    """
+    Тестирует функционал метода создания списка из 5
+    последних успешных операций клиента (make_output_list).
+    """
+    correct_output_list = [
+        {
+            "id": 482520625,
+            "state": "EXECUTED",
+            "date": "2019-11-13T17:38:04.800051",
+            "operationAmount": {
+                "amount": "62814.53",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 38611439522855669794",
+            "to": "Счет 46765464282437878125"
+        },
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 667307132,
+            "state": "EXECUTED",
+            "date": "2019-07-13T18:51:29.313309",
+            "operationAmount": {
+                "amount": "97853.86",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 1308795367077170",
+            "to": "Счет 96527012349577388612"
+        },
+        {
+            "id": 414894334,
+            "state": "EXECUTED",
+            "date": "2019-06-30T15:11:53.136004",
+            "operationAmount": {
+                "amount": "95860.47",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 59956820797131895975",
+            "to": "Счет 43475624104328495820"
+        },
+        {
+            "id": 509552992,
+            "state": "EXECUTED",
+            "date": "2019-04-19T12:02:30.129240",
+            "operationAmount": {
+                "amount": "81513.74",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Maestro 9171987821259925",
+            "to": "МИР 2052809263194182"
+        }
+    ]
+
+    data_process.data_screening(test_data)
+    data_process.data_sort()
+    data_process.make_output_list()
+    assert data_process.list_for_public == correct_output_list
+
+
+def test_print_info_mask(data_process):
+    """
+    Тестирует функционал метода маскирования данных и вывода на печать (print_info).
+    """
+    pass
+
+def test_print_info(data_process):
+    """
+    Тестирует функционал метода маскирования данных и вывода на печать (print_info).
+    """
+    pass
